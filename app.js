cd20ccec97a2579fb5fc278e578c9edd74c64803
Nguyen-The-Lap <91041708+Nguyen-The-Lap@users.noreply.github.com>
2025-02-13 15:22:27 +0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const cors = require('cors');
-
-// Thêm middleware CORS
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-})); 
